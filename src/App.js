0138e1e1dec4522170ac9c5271792f9aea838414
Trend20 <enockomondi305@gmail.com>
2020-05-06 16:35:30 -0400

    image commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
-function App() {
+import Card from './Card';
+import image from './img/web.jpeg';
+function App(props) {
   return (
     <div className="App">
-      
+      <div className="row">
+        <div className="col-lg-12">
+          <Card 
+            image={image}
+            title="Programming in Javascript"
+            description="A video on how to get started with Javascript"
+            link=""
+          />
+        </div>
+      </div>
     </div>
   );
 }
